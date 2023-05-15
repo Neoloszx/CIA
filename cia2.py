aa91fa87377b1a6c9af648de3fa0ae8c20e35f9f
bptlfb <107214184+bptlfb@users.noreply.github.com>
2023-05-15 17:26:34 +0200

    Create cia2.py

---
+++ cia2.py
@@ -0,0 +1,25 @@
+import cia
+import subprocess
+
+# Initialisation du but
+but = [1,0,1,0,1,0,0,1]
+
+# Exécution du script cia.py
+result = subprocess.run(['python', 'cia.py'], capture_output=True)
+etat_final = cia.Eventgen()
+loop = 0
+
+while (but != etat_final):
+    result
+    etat_final = cia.Eventgen()
+    #print(etat_final)
+    loop += 1
+
+print("Etat final est :", etat_final)
+print("Nbre itération :", loop)
+    
+
+
+
+
+
