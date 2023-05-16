ffcf17687c843d47b791445b11784cc889e88a4f
Neoloszx <125834296+Neoloszx@users.noreply.github.com>
2023-05-16 11:18:09 +0200

    Update CIA5.py

--- CIA3.py
+++ CIA3.py
@@ -48,7 +48,7 @@ char4 = Admin("IT Support")
 char5 = Employee("Directeur")
 
  
-#admin_connect,server_status,client_connect,demand_vivement,depot_cheque,employee_connect,vivement_approuved,cheque_approuved
+#admin_connect,server_status,client_connect,demand_vivement,depot_cheque,vivement_approuved,cheque_approuved
 
 def Eventgen():
     listx=[0,0,0,0,0,0,0,0]
@@ -66,6 +66,8 @@ def Eventgen():
         if(char4.server_start!=True):
             print("Admin didn't start server")
             print(listx)
+            return listx
+            
         else:
             listx[1]=1
             print("Server start by Admin All operation is permitted\n")
@@ -89,12 +91,12 @@ def Eventgen():
                 print(listx)
                 char5.Commit_cheque=random.choice([True,False])
                 if(char5.Commit_cheque!=True):
-                    listx[7]=0
+                    listx[6]=0
                     print("Director rejected the cheque \n")
                     print(listx)
                     return listx
                 else:#char5.Approuve_Pret=True
-                    listx[7]=1
+                    listx[6]=1
                     print("Director appouve the cheque \n")
                     print(listx)
                     char1.virement=random.choice([True,False])
@@ -107,12 +109,12 @@ def Eventgen():
                         print(listx)
                         char5.vivement_Approuve=random.choice([True,False])
                         if(char5.vivement_Approuve!=True):
-                            listx[6]=0
+                            listx[5]=0
                             print("Director rejected the vivement\n")
                             print(listx)
                             return listx
                         else:#char5.vivement_Approuve=True
-                            listx[6]=1
+                            listx[5]=1
                             print("Director appouve the cheque \n")
                             print(listx)
                             return listx
@@ -120,56 +122,43 @@ def generate_possibilities():
     possibilities = []
     for admin_connect in [False, True]:
         for server_status in [False, True]:
-            for client_connect in [False, True]:
-                for demand_virement in [False, True]:
-                    for depot_cheque in [False, True]:
-                        for employee_connect in [False, True]:
-                            for vivement_approuve in [False, True]:
-                                for cheque_approuve in [False, True]:
-                                    # Apply conditions and exclude invalid possibilities
-                                    if (admin_connect and server_status) or (not admin_connect):
-                                        if client_connect:
-                                            if depot_cheque:
-                                                if cheque_approuve:
-                                                    if demand_virement:
-                                                        if vivement_approuve:
-                                                            possibilities.append([
-                                                                admin_connect,
-                                                                server_status,
-                                                                client_connect,
-                                                                demand_virement,
-                                                                depot_cheque,
-                                                                employee_connect,
-                                                                vivement_approuve,
-                                                                cheque_approuve
-                                                            ])
-                                        else:
-                                            possibilities.append([
-                                                admin_connect,
-                                                server_status,
-                                                client_connect,
-                                                demand_virement,
-                                                depot_cheque,
-                                                employee_connect,
-                                                vivement_approuve,
-                                                cheque_approuve
-                                            ])
-    
-    return possibilities                         
+            if not server_status or admin_connect:
+                for client_connect in [False, True]:
+                    if not client_connect or not depot_cheque or not cheque_approuve or not demand_virement or not vivement_approuve:
+                        for demand_virement in [False, True]:
+                            if not demand_virement or not depot_cheque:
+                                for depot_cheque in [False, True]:
+                                    if not depot_cheque or not cheque_approuve:
+                                        for employee_connect in [False, True]:
+                                            for vivement_approuve in [False, True]:
+                                                if not vivement_approuve or demand_virement:
+                                                    for cheque_approuve in [False, True]:
+                                                        possibilities.append([
+                                                            admin_connect,
+                                                            server_status,
+                                                            client_connect,
+                                                            demand_virement,
+                                                            depot_cheque,
+                                                            employee_connect,
+                                                            vivement_approuve,
+                                                            cheque_approuve
+                                                        ])
+
+    return possibilities
+
+
+
+
+
+                         
 if __name__ == '__main__':
-    print("[admin_connect,server_status,client_connect,demand_vivement,depot_cheque,employee_connect,vivement_approuved,cheque_approuved]")
+    print("[admin_connect,server_status,client_connect,demand_vivement,depot_cheque,vivement_approuved,cheque_approuved]")
     Eventgen()
     possibilities = generate_possibilities()
-    
-    total_possibilities = len(possibilities)
-    print("Total number of possibilities:", total_possibilities)
-
-    #print("How many event you want to generate ?")
-    #inum=int(input())
-    #listforshow=[]
-    #for i in range (inum-1):
-        #listforshow[i].append(Eventgen())
-       # print(listforshow[i])
+    possibilities_count = len(generate_possibilities())
+    for possibility in possibilities:
+        print(possibility)
+    print("Possibility_count : ",possibilities_count)
         
     
                     
